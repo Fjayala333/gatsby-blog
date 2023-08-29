146745ab0e489de97ff0c72d85b220fd9658a4ce
unknown <francisco_a333@hotmail.com>
2023-08-29 07:16:10 -0700

    fixed styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,6 +28,7 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
+          backgroundColor: "mediumpurple",
           margin: `0 auto`,
           maxWidth: `960`,
           padding: `0px 1.0875rem 1.45rem`,
